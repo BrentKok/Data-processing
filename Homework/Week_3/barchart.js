60f45fc8d7529296b5dc166110de14647fd0c508
Brent <brentkok@hotmail.com>
2018-02-23 18:16:47 +0100

    js

--- Homework/Week_3/barchart.js
+++ Homework/Week_3/barchart.js
@@ -1,32 +1,46 @@
-d3.json("data.json", function(json) {
-      x.domain(data.map(function(d) { return d.name; }));
-      y.domain([0, d3.max(data, function(d) { return d.value; })]);
+// Gets the data from data.json
+d3.json("data.json", function(error, data) {
+  if (error) throw error;
 
-var svg = d3.select("body").append("svg")
-	.attr("height", "100%")
-	.attr("width", "100%");
+  // Formats the data
+  data.forEach(function(d) {
+      d.date = parseTime(d.date);
+      d.close = +d.close;
+  });
+
+  // Makes sure that the range of the data fits the scale
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain([0, d3.max(data, function(d) { return d.close; })]);
+
+  // Adding the x Axis
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  // Applies the text label for the X-axis
+  svg.append("text")             
+      .attr("transform",
+            "translate(" + (width/2) + " ," + 
+                           (height + margin.top + 20) + ")")
+      .style("text-anchor", "middle")
+      .text("Date");
+
+  // Adding the y Axis
+  svg.append("g")
+      .call(d3.axisLeft(y));
+
+  // Applies the text label for the X-axis
+  svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Rain"); 
+}}
 
 
-      svg.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis);
 
-      svg.append("g")
-          .attr("class", "y axis")
-          .call(yAxis);
 
-      svg.selectAll(".bar")
-          .data(data)
-        .enter().append("rect")
-          .attr("class", "bar")
-          .attr("x", function(d) { return x(d.name); })
-          .attr("y", function(d) { return y(d.value); })
-          .attr("height", function(d) { return height - y(d.value); })
-          .attr("width", x.rangeBand());
-    });
 
-    function type(d) {
-      d.value = +d.value; // coerce to number
-      return d;
-    }
+
