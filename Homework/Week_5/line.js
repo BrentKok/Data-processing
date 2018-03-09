339971e34caff9d3f33b08b18438cfd1d03bfee9
BrentKok <brentkok.bk@gmail.com>
2018-03-09 14:28:20 +0100

    js

--- Homework/Week_5/line.js
+++ Homework/Week_5/line.js
@@ -1,25 +1,13 @@
 // Brent Kok
 // 10725156
 // Data Processing Week 5 - Line Graph
-// https://bl.ocks.org/jqadrad/a58719d82741b1642a2061c071ae2375
-
 
 window.onload = function() {
 	
 	// Setting margins for the place of the chart
-	var svg = d3.select('svg'),
-		margin = {top: 30, right: 40, bottom: 30, left: 50},
+	var	margin = {top: 30, right: 40, bottom: 30, left: 50},
 		width = 600 - margin.left - margin.right,
-		height = 270 - margin.top - margin.bottom,
-  		g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-	// Graph of the title
-	g.append('text')
-		.attr('x', (width - 2))             
-		.attr('y', 0 - (margin.top / 3))
-		.attr('text-anchor', 'middle')  
-		.style('font-size', '16px') 
-		.text('Temperature De Bilt & Berkhout');
+		height = 270 - margin.top - margin.bottom;
 
 	// Transform date
 	var	parseDate = d3.time.format("%Y%m%d").parse;
@@ -30,16 +18,10 @@ window.onload = function() {
 
 	var color = d3.scale.category10();
 
-	var	xAxis = d3.svg.axis().scale(x)
-		.orient("bottom");
-
-	var	yAxis = d3.svg.axis().scale(y)
-		.orient("left");
-
 	// Initialise line
 	var	line = d3.svg.line()
-		.x(function(d) { return x(d.date); })
-		.y(function(d) { return y(d.temp); });
+		.x(function(d) { console.log(d.date, d.value); return x(parseDate(d.date)); })
+		.y(function(d) { return y(d.value); });
 	
 	// Create svg element
 	var	svg = d3.select("body").append("svg")
@@ -48,13 +30,7 @@ window.onload = function() {
 		.append("g")
 			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-
 	d3.json("line.json", function(error, data) {
-		//data.forEach(function(d){
-			//d.date = parseDate(d.date);
-			//d.values = +d.values;
-	//	});
-
 		if (error) throw error;
 
 		// Log data as a check
@@ -65,110 +41,88 @@ window.onload = function() {
 			return key !== "date" && key !== "Max" && key !== "Min" && key !== "Average";
 		}));
 
-		var Station = color.domain().map(function(name) {
+		var Stations = color.domain().map(function(name) {
 			return {
-				name: name,
+				d: d,
 				values: data.map(function(d) {
-					return {date: d.date, temp: +d[name]};
+					return {date: parseDate(d.date), values: +d[d]};
 				})
 			};
 		});
-
+		console.log(Stations)
+		
 		// Set the domain for X
-		x.domain(d3.extent(data, function(d) { return d.date; }));
+		x.domain(d3.extent(data["260"]["Max"], function(d) { return parseDate(d.date); }));
 		
 		// Set the domain for Y
 		y.domain([
-			d3.min(Station, function(c) { return d3.min(c.values, function(v) { return v.temp; }); }),
-			d3.max(Station, function(c) { return d3.max(c.values, function(v) { return v.temp; }); })
+			d3.min(d3.entries(data["260"]), function(c) { return d3.min(c.value, function(v) { return v.value; }); }),
+			d3.max(d3.entries(data["260"]), function(c) { return d3.max(c.value, function(v) { return v.value; }); })
 		]);
 
+		// Scale X and Y axis
+		var	xAxis = d3.svg.axis().scale(x)
+			.orient("bottom");
 
-	/*
-	svg.selectAll("*").remove();
+		var	yAxis = d3.svg.axis().scale(y)
+			.orient("left");
 
-	var legend = svg.selectAll('g')
-		.data(Station)
-		.enter()
-	.append('g')
-		.attr('class', 'legend');
-
-	legend.append('rect')
-		.attr('x', width - 20)
-		.attr('y', function(d, i){ return i * 20;})
-		.attr('width', 10)
-		.attr('height', 10)
-		.style('fill', function(d){
-			return color(d.name);
-		});
-
-	legend.append('text')
-		.attr('x', width - 8)
-		.attr('y', function(d, i){ return (i * 20) + 9;})
-		.text(function(d){ return d.name; });
-
-	legend
-			.on("click", function(d){
-				reDraw(d.name);
-			});
-	*/
-	
-	// Setting the X-axis including attributes
+		// Setting the X-axis including attributes
 		svg.append("g")		
 			.attr("class", "x axis")
 			.attr("transform", "translate(0," + height + ")")
 			.call (xAxis)
 		.append("text")
 			.attr("transform", "rotate(0)")
-			.attr("x", -15)
+			.attr("x", -25)
 			.attr("dx", ".71em")
 			.style("text-anchor", "end")
-			.text("Date");
+			.text("");
 
-	    // Setting the Y-axis including attributes
+		// Setting the Y-axis including attributes
 		svg.append("g")
 			.attr("class", "y axis")
 			.call(yAxis)
 		.append("text")
 			.attr("transform", "rotate(-90)")
-			.attr("y", -15)
+			.attr("y", 4)
 			.attr("dy", ".71em")
 			.style("text-anchor", "end")
 			.text("Temperature in C");
 
 		var Station = svg.selectAll(".Station")
-			.data(Station)
+			.data(["Min", "Average", "Max"])
 			.enter().append("g")
-			.attr("class", "Station");
+			.attr("class", "City");
 
-	    // Draw lines
+		// Draw lines
 		Station.append("path")
 			.attr("class", "line")
 			.attr("d", function(d) { 
-				return line(d.values); 
+				return line(data["260"][d]); 
 			})
 			.style("stroke", function(d) { 
-				return color(d.name); 
+				return color(d); 
 			});
 
 		Station.append("text")
 			.datum(function(d) { 
 				return { 
-					name: d.name, 
-					value: d.values[d.values.length - 1]
+					name: d, 
+					value: d
 				}; 
 			})
 			.attr("transform", function(d) { 
-				return "translate(" + x(d.value.date) + "," + y(d.value.temp) + ")"; 
+				return "translate(" + x(d.value.date) + "," + y(d.value.value) + ")"; 
 			})
-			.attr("x", 3)
+			.attr("x", 250)
 			.attr("dy", ".35em")
 			.text(function(d) { 
 				return d.name; 
 			});
 
 		Station.selectAll("circle")
-			.data(function(d){return d.values})
+			.data(function(d){return d})
 			.enter()
 			.append("circle")
 			.attr("r", 3)
@@ -176,10 +130,11 @@ window.onload = function() {
 				return x(d.date); 
 			})
 			.attr("cy", function(d) { 
-				return y(d.values); 
+				return y(d.value); 
 			})
 			.style("fill", function(d,i,j) { 
-				return color(Station[j].name); 
+				return color(Station.d); 
 			});
-	});
-}		
+	
+  });
+}
